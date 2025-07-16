32baa8389188922f3289dd59d77691f37c239b8d
object-wty <1483234229@qq.com>
2025-07-16 20:34:41 +0800

    添加项目文件。

---
+++ LuanYu_ArtisanBeer/ArtisanBeerMissonView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.MissionViews;
+using TaleWorlds.ObjectSystem;
+
+namespace LuanYu_ArtisanBeer
+{
+    public class ArtisanBeerMissonView : MissionView
+    {
+        
+
+        public override void OnMissionTick(float dt)
+        {
+
+            if (TaleWorlds.InputSystem.Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q))
+            {
+                if (!(Mission.Mode is MissionMode.Battle or MissionMode.Stealth))
+                    return;
+
+                //在战斗过程中 按Q,消耗一个 artisan beer,回血20
+                TaleWorlds.CampaignSystem.Roster.ItemRoster itemRoster = MobileParty.MainParty.ItemRoster;
+                ItemObject artisanBeerObject = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
+                if (itemRoster.GetItemNumber(artisanBeerObject) <= 0)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage("Artisan Beer 库存不足"));
+                    return;
+                }
+
+                if (Mission.MainAgent.Health >= Mission.MainAgent.HealthLimit)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage("血量满,不可以使用"));
+                    return;
+                }
+
+                itemRoster.AddToCounts(artisanBeerObject, -1);
+
+                Mission.MainAgent.Health += 20;
+
+                InformationManager.DisplayMessage(new InformationMessage($"加了20滴血"));
+
+            }
+
+        }
+
+    }
+}
---
+++ LuanYu_ArtisanBeer/SubModule.cs
@@ -0,0 +1,36 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+
+namespace LuanYu_ArtisanBeer
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        public override void OnMissionBehaviorInitialize(Mission mission)
+        {
+            base.OnMissionBehaviorInitialize(mission);
+            mission.AddMissionBehavior(new ArtisanBeerMissonView());
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+
+        
+
+    }
+}
