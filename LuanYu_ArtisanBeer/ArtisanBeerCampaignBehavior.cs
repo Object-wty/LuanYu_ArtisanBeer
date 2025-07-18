90de7b80d615e8f3f6af782e883762e10b0df86c
object-wty <1483234229@qq.com>
2025-07-18 20:11:17 +0800

    EP6 让NPC化身酒贩子：逆向NPC对话系统实现精酿啤酒交易

--- LuanYu_ArtisanBeer/ArtisanBeerCampaignBehavior.cs
+++ LuanYu_ArtisanBeer/ArtisanBeerCampaignBehavior.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using SandBox.Conversation;
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.AgentOrigins;
+using TaleWorlds.CampaignSystem.Conversation;
 using TaleWorlds.CampaignSystem.Encounters;
+using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.Settlements.Locations;
 using TaleWorlds.CampaignSystem.Settlements.Workshops;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.ObjectSystem;
 
 namespace LuanYu_ArtisanBeer
 {
@@ -18,6 +25,44 @@ namespace LuanYu_ArtisanBeer
         {
             CampaignEvents.DailyTickTownEvent.AddNonSerializedListener(this, OnDailyTickTown);
             CampaignEvents.LocationCharactersAreReadyToSpawnEvent.AddNonSerializedListener(this, new Action<Dictionary<string, int>>(this.LocationCharactersAreReadyToSpawn));
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, new Action<CampaignGameStarter>(this.OnSessionLaunched));
+        }
+
+        private void OnSessionLaunched(CampaignGameStarter starter)
+        {
+            this.AddArtisanWokerDialogs(starter);
+        }
+
+        private void AddArtisanWokerDialogs(CampaignGameStarter campaignGameStarter)
+        {
+            campaignGameStarter.AddDialogLine("artisanbeer_owner_begin", "start", "artisanbeer_npc_player", "嘿，朋友！想来点特别的吗？我们这有上等的精酿啤酒，200第纳尔一杯。要尝尝吗？", 
+            ()=> CharacterObject.OneToOneConversationCharacter == PlayerEncounter.LocationEncounter.Settlement.Culture.CaravanMaster, null, 100, null);
+            campaignGameStarter.AddPlayerLine("artisanbeer_buy_one", "artisanbeer_npc_player", "artisanbeer_player_buy", "当然，给我来一杯", null, 
+            () =>
+            {
+                TaleWorlds.CampaignSystem.Roster.ItemRoster itemRoster = MobileParty.MainParty.ItemRoster;
+                ItemObject artisanBeerObject = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
+                itemRoster.AddToCounts(artisanBeerObject, 1);
+                Hero.MainHero.ChangeHeroGold(-200);
+                
+            }
+            , 100, new ConversationSentence.OnClickableConditionDelegate(this.BuyArtisanBeer_clickable_condition), null);
+            campaignGameStarter.AddDialogLine("artisanbeer_buy_one_2", "artisanbeer_player_buy", "end", "明智的选择！这可是用高地麦芽酿的，包你回味无穷！\"", 
+                null, null, 100, null);
+            campaignGameStarter.AddPlayerLine("artisanbeer_no_buy", "artisanbeer_npc_player", "artisanbeer_nobuy", "不用了，我对啤酒没兴趣", null, null, 100, null, null);
+            campaignGameStarter.AddDialogLine("artisanbeer_no_buy_2", "artisanbeer_nobuy", "close_window", "哼，不识货的家伙...下次可别后悔。", null, null, 100, null);
+        }
+
+        private bool BuyArtisanBeer_clickable_condition(out TextObject explanation)
+        {
+            
+            if (Hero.MainHero.Gold < 200)
+            {
+                explanation = new TextObject("金币不足200，无法购买", null);
+                return false;
+            }
+            explanation = TextObject.Empty;
+            return true;
         }
 
         private void LocationCharactersAreReadyToSpawn(Dictionary<string, int> unusedUsablePointCount)
