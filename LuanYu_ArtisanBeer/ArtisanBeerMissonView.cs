37a4815296034ad7b4980c62536d2c268ac752ac
object-wty <1483234229@qq.com>
2025-07-19 10:48:24 +0800

    EP8 在战斗中显示精酿啤酒库存 - UI界面制作+MVVM模式代码编写

--- LuanYu_ArtisanBeer/ArtisanBeerMissonView.cs
+++ LuanYu_ArtisanBeer/ArtisanBeerMissonView.cs
@@ -1,49 +1,57 @@
 ﻿using System;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Core;
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.GauntletUI.Data;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.GauntletUI.Mission;
+using TaleWorlds.MountAndBlade.View;
 using TaleWorlds.MountAndBlade.View.MissionViews;
+using TaleWorlds.MountAndBlade.View.MissionViews.Singleplayer;
+using TaleWorlds.MountAndBlade.ViewModelCollection.HUD;
 using TaleWorlds.ObjectSystem;
 
 namespace LuanYu_ArtisanBeer
 {
-    public class ArtisanBeerMissonView : MissionView
+
+    [OverrideView(typeof(MissionAgentLockVisualizerView))]
+    public class ArtisanBeerMissonView :  MissionGauntletBattleUIBase
     {
-        
+        private ArtisanBeerVM _dataSource;
+        private GauntletLayer _layer;
+        private IGauntletMovie _movie;
 
         public override void OnMissionTick(float dt)
         {
 
-            if (TaleWorlds.InputSystem.Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q))
-            {
-                if (!(Mission.Mode is MissionMode.Battle or MissionMode.Stealth))
-                    return;
-
-                //在战斗过程中 按Q,消耗一个 artisan beer,回血20
-                TaleWorlds.CampaignSystem.Roster.ItemRoster itemRoster = MobileParty.MainParty.ItemRoster;
-                ItemObject artisanBeerObject = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
-                if (itemRoster.GetItemNumber(artisanBeerObject) <= 0)
-                {
-                    InformationManager.DisplayMessage(new InformationMessage("Artisan Beer 库存不足"));
-                    return;
-                }
-
-                if (Mission.MainAgent.Health >= Mission.MainAgent.HealthLimit)
-                {
-                    InformationManager.DisplayMessage(new InformationMessage("血量满,不可以使用"));
-                    return;
-                }
-
-                itemRoster.AddToCounts(artisanBeerObject, -1);
-
-                Mission.MainAgent.Health += 20;
-
-                InformationManager.DisplayMessage(new InformationMessage($"加了20滴血"));
-
-            }
+            _dataSource?.DrinkArtisanBeer();
+            
 
         }
 
+        
+
+        protected override void OnCreateView()
+        {
+            this._dataSource = new ArtisanBeerVM(Mission);
+            this._layer = new GauntletLayer(1, "GauntletLayer", false);
+            this._movie = this._layer.LoadMovie("ArtisanBeerCount_GUI", this._dataSource);
+            if (base.Mission.Mode != MissionMode.Conversation && base.Mission.Mode != MissionMode.CutScene)
+            {
+                base.MissionScreen.AddLayer(this._layer);
+            }
+        }
+
+        protected override void OnDestroyView()
+        {
+            if (base.Mission.Mode != MissionMode.Conversation && base.Mission.Mode != MissionMode.CutScene)
+            {
+                base.MissionScreen.RemoveLayer(this._layer);
+            }
+            this._dataSource = null;
+            this._movie = null;
+            this._layer = null;
+        }
     }
 }
---
+++ LuanYu_ArtisanBeer/ArtisanBeerVM.cs
@@ -0,0 +1,101 @@
+﻿using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.ObjectSystem;
+
+namespace LuanYu_ArtisanBeer
+{
+    public class ArtisanBeerVM :ViewModel
+    {
+        private readonly Mission _mission;
+        private bool _isVisible;
+        private int _artisanBeerCount;
+
+        public ArtisanBeerVM(Mission mission)
+        {
+            this._mission = mission;
+            OnMissionChanged();
+            TaleWorlds.CampaignSystem.Roster.ItemRoster itemRoster = MobileParty.MainParty.ItemRoster;
+            ItemObject artisanBeerObject = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
+            ArtisanBeerCount = itemRoster.GetItemNumber(artisanBeerObject);
+        }
+
+        private void OnMissionChanged()
+        {
+            if (_mission.Mode != MissionMode.Conversation && _mission.Mode != MissionMode.CutScene)
+            {
+                IsVisible = true;
+            }
+            else
+            {
+                IsVisible = false;
+            }
+        }
+
+        [DataSourceProperty]
+        public bool IsVisible
+        {
+            get
+            {
+                return this._isVisible;
+            }
+            set
+            {
+                if (value != this._isVisible)
+                {
+                    this._isVisible = value;
+                    base.OnPropertyChangedWithValue(value, "IsVisible");
+                }
+            }
+        }
+
+        [DataSourceProperty]
+        public int ArtisanBeerCount
+        {
+            get
+            {
+                return this._artisanBeerCount;
+            }
+            set
+            {
+                if (value != this._artisanBeerCount)
+                {
+                    this._artisanBeerCount = value;
+                    base.OnPropertyChangedWithValue(value, "ArtisanBeerCount");
+                }
+            }
+        }
+
+        public void DrinkArtisanBeer()
+        {
+            if (TaleWorlds.InputSystem.Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q))
+            {
+                if (!(this._mission.Mode is MissionMode.Battle or MissionMode.Stealth))
+                    return;
+
+                //在战斗过程中 按Q,消耗一个 artisan beer,回血20
+                TaleWorlds.CampaignSystem.Roster.ItemRoster itemRoster = MobileParty.MainParty.ItemRoster;
+                ItemObject artisanBeerObject = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
+                if (itemRoster.GetItemNumber(artisanBeerObject) <= 0)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage("Artisan Beer 库存不足"));
+                    return;
+                }
+
+                if (this._mission.MainAgent.Health >= this._mission.MainAgent.HealthLimit)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage("血量满,不可以使用"));
+                    return;
+                }
+
+                itemRoster.AddToCounts(artisanBeerObject, -1);
+                ArtisanBeerCount--;
+                this._mission.MainAgent.Health += 20;
+
+                InformationManager.DisplayMessage(new InformationMessage($"加了20滴血"));
+
+            }
+        }
+    }
+}
